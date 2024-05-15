f8d42e5bf0e483239a13da4b4a5b575bfeafe960
Taoofshinsei <taoofshinsei@yahoo.com>
2024-05-15 10:24:38 -0400

    Fixed UI not updating on gamma change.

--- src/components/GammaSettings.js
+++ src/components/GammaSettings.js
@@ -2,40 +2,43 @@ import React, { useState } from "react";
 import { CCol, CFormRange, CContainer, CRow } from "@coreui/react";
 
 export function GammaSettings({baseOptions, onUpdate}) {
-    const [gamma, setGamma] = useState(baseOptions['gamma']);
+    const [gamma0, setGamma0] = useState(baseOptions['gamma'][0]);
+    const [gamma1, setGamma1] = useState(baseOptions['gamma'][1]);
+    const [gamma2, setGamma2] = useState(baseOptions['gamma'][2]);
+    const [gamma3, setGamma3] = useState(baseOptions['gamma'][3]);
+    const [gamma4, setGamma4] = useState(baseOptions['gamma'][4]);
 
-    function updateGamma(e, index) {
-        var newGamma = gamma;
-        newGamma[index] = e.target.value;
-        setGamma(newGamma);
-        onUpdate('gamma', newGamma);
+    function updateGamma(e, func) {
+        func(parseFloat(e.target.value));
+        onUpdate('gamma', [gamma0, gamma1, gamma2, gamma3, gamma4]);
+        console.log([gamma0, gamma1, gamma2, gamma3, gamma4]);
     }
 
     return (
         <CContainer>
             <CRow>
                 <CCol>
-                    <CFormRange id="gamma0" label={"Gamma 1: " + gamma[0] * 10} min={-1} max={1} defaultValue={gamma[0]} onChange={e => updateGamma(e, 0)} step={0.1} />
+                    <CFormRange id="gamma0" label={"Gamma 1: " + gamma0 * 10} min={-1} max={1} defaultValue={gamma0} onChange={e => updateGamma(e, setGamma0)} step={0.1} />
                 </CCol>
             </CRow>
             <CRow>
                 <CCol>
-                    <CFormRange id="gamma1" label={"Gamma 2: " + gamma[1] * 10} min={-1} max={1} defaultValue={gamma[1]} onChange={e => updateGamma(e, 1)} step={0.1} />
+                    <CFormRange id="gamma1" label={"Gamma 2: " + gamma1 * 10} min={-1} max={1} defaultValue={gamma1} onChange={e => updateGamma(e, setGamma1)} step={0.1} />
                 </CCol>
             </CRow>
             <CRow>
                 <CCol>
-                    <CFormRange id="gamma2" label={"Gamma 3: " + gamma[2] * 10} min={-1} max={1} defaultValue={gamma[2]} onChange={e => updateGamma(e, 2)} step={0.1} />
+                    <CFormRange id="gamma2" label={"Gamma 3: " + gamma2 * 10} min={-1} max={1} defaultValue={gamma2} onChange={e => updateGamma(e, setGamma2)} step={0.1} />
                 </CCol>
             </CRow>
             <CRow>
                 <CCol>
-                    <CFormRange id="gamma3" label={"Gamma 4: " + gamma[3] * 10} min={-1} max={1} defaultValue={gamma[3]} onChange={e => updateGamma(e, 3)} step={0.1} />
+                    <CFormRange id="gamma3" label={"Gamma 4: " + gamma3 * 10} min={-1} max={1} defaultValue={gamma3} onChange={e => updateGamma(e, setGamma3)} step={0.1} />
                 </CCol>
             </CRow>
             <CRow>
                 <CCol>
-                    <CFormRange id="gamma4" label={"Gamma 5: " + gamma[4] * 10} min={-1} max={1} defaultValue={gamma[4]} onChange={e => updateGamma(e, 4)} step={0.1} />
+                    <CFormRange id="gamma4" label={"Gamma 5: " + gamma4 * 10} min={-1} max={1} defaultValue={gamma4} onChange={e => updateGamma(e, setGamma4)} step={0.1} />
                 </CCol>
             </CRow>
         </CContainer>
